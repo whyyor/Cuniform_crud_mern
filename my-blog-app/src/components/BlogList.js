49d3729b1c67429c11fde3acf4e4688fbb3071e0
whyyor <whyyor@gmail.com>
2023-04-15 03:08:12 +0530

    add searchbar

--- my-blog-app/src/components/BlogList.js
+++ my-blog-app/src/components/BlogList.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { getBlogs } from "../actions/blogActions";
 import { Button, Card } from "react-bootstrap";
 import { Link } from "react-router-dom";
@@ -11,6 +11,21 @@ const BlogList = () => {
   const blogs = useSelector((state) => state.blogs);
   const location = useLocation();
 
+  // search
+  const [searchTerm, setSearchTerm] = useState("");
+  const [filteredBlogs, setFilteredBlogs] = useState([]);
+
+  const filterBlogs = () => {
+    if (!searchTerm) {
+      setFilteredBlogs(blogs.blogs);
+    } else {
+      const filtered = blogs.blogs.filter((blog) =>
+        blog.title.toLowerCase().includes(searchTerm.toLowerCase())
+      );
+      setFilteredBlogs(filtered);
+    }
+  };
+
   const handleDelete = (id) => {
     dispatch(deleteBlog(id));
   };
@@ -21,16 +36,27 @@ const BlogList = () => {
 
   useEffect(() => {
     dispatch(getBlogs());
-  }, [dispatch, blogs]);
+    filterBlogs();
+  }, [dispatch, blogs, searchTerm]);
 
   return (
-    <div>
-      {location.pathname !== "/create-blog" && (
-        <Link to="/create-blog">
-          <Button variant="primary">Create Blog</Button>
-        </Link>
-      )}
-      {blogs.blogs.map((blog) => (
+    <div style={{ width: "60%" }} className="mx-auto">
+      <div className="d-flex justify-content-between mb-3">
+        <input
+          type="text"
+          placeholder="Search by title"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          className="form-control mb-3"
+          style={{width: "60%"}}
+        />
+        {location.pathname !== "/create-blog" && (
+          <Link to="/create-blog">
+            <Button variant="primary">Create Blog</Button>
+          </Link>
+        )}
+      </div>
+      {filteredBlogs.map((blog) => (
         <Card key={blog.id} className="my-3">
           <Card.Body>
             <div className="d-flex justify-content-between align-items-center">
