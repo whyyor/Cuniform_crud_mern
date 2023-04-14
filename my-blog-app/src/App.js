5aab609d12f181ef555312a910b8a4877cf43e40
whyyor <whyyor@gmail.com>
2023-04-15 01:10:32 +0530

    add delete functionality

--- my-blog-app/src/App.js
+++ my-blog-app/src/App.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getBlogs } from "./actions/blogActions";
 import BlogList from "./components/BlogList";
+import { Container, Col } from "react-bootstrap";
 
 function App() {
   const dispatch = useDispatch();
@@ -12,12 +13,15 @@ function App() {
     dispatch(getBlogs());
   }, [dispatch]);
 
+
   return (
-    <div className="App">
+    <Container className="d-flex flex-column align-items-center py-3">
       <h1>All Blogs</h1>
-      <BlogList blogs={blogs.blogs} />
-    </div>
+      <Col xs={12}>
+        <BlogList blogs={blogs.blogs} />
+      </Col>
+    </Container>
   );
 }
 
-export default React.memo(App);
+export default App;
--- my-blog-app/src/components/BlogList.js
+++ my-blog-app/src/components/BlogList.js
@@ -1,12 +1,28 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Blog from "./Blog";
 import { Button, Card } from "react-bootstrap";
+import { useDispatch } from "react-redux";
+import { deleteBlog, updateBlog } from "../actions/blogActions";
 
 const BlogList = ({ blogs }) => {
+  const dispatch = useDispatch();
+
+  const handleDelete = (id) => {
+    dispatch(deleteBlog(id));
+  };
+
+  const handleUpdate = (id, updatedBlog) => {
+    dispatch(updateBlog(updatedBlog, id));
+  };
+
+  useEffect(() => {
+    console.log(blogs.map((blog) => `${blog.id}`));
+  }, [blogs]);
+
   return (
     <div>
       {blogs.map((blog) => (
-        <Card key={blog._id} className="my-3">
+        <Card key={blog.id} className="my-3">
           <Card.Body>
             <div className="d-flex justify-content-between align-items-center">
               <div>
@@ -16,10 +32,25 @@ const BlogList = ({ blogs }) => {
                 </Card.Subtitle>
               </div>
               <div>
-                <Button variant="outline-primary" className="mx-2">
+                <Button
+                  variant="outline-primary"
+                  className="mx-2"
+                  onClick={() =>
+                    handleUpdate(blog.id, {
+                      title: "New Title",
+                      description: "New Description",
+                      category: "New Category",
+                    })
+                  }
+                >
                   Edit
                 </Button>
-                <Button variant="outline-danger">Delete</Button>
+                <Button
+                  variant="outline-danger"
+                  onClick={() => handleDelete(blog.id)}
+                >
+                  Delete
+                </Button>
               </div>
             </div>
             <Card.Text>{blog.description}</Card.Text>
